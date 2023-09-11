ddfa1fe0ff546842e173192f60ddff91eea04bfc
Samuel Fu <sfu@c2r4s14.42singapore.sg>
2023-09-12 02:18:18 +0800

    updated for records

---
+++ libft/ft_memset.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sfu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 01:17:57 by sfu               #+#    #+#             */
+/*   Updated: 2023/09/12 02:10:20 by sfu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void*	ft_memset(void* a, int c, size_t n)
+{
+	unsigned char*	arr;
+	unsigned char 	ac;
+
+	arr = a;
+	ac = c;
+	while(n>0)
+	{
+		arr[n-1] = ac;
+		n--;
+	}
+	return(a);
+}
+
+int main() 
+{
+	char buffer[10];
+	char *result;
+
+	result = ft_memset(buffer, 'A', 5);
+	write(STDOUT_FILENO, result, 5);
+	write(STDOUT_FILENO, "\n", 1);
+
+	return 0;
+}
