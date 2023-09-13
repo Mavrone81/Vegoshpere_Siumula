ad3f5eb4b42adb8fe3d4a8523d8bf5135015247d
Samuel Fu <sfu@c2r4s14.42singapore.sg>
2023-09-13 08:34:40 +0800

    Progress but slow

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sfu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 21:11:36 by sfu               #+#    #+#             */
+/*   Updated: 2023/09/13 08:34:14 by sfu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void*	ft_memcpy(void* des, void* src, size_t n)
+{
+	int	i;
+	char*	c_des;
+	char*	c_src;
+
+	i = 0;
+	c_des = (char*) des;
+	c_src = (char*) src;
+	if (des == NULL)
+		return(NULL);
+	while (i < n)
+	{
+		c_des[i] = c_src[i];
+		i++;
+	}
+	return(des);
+}
+
+typedef struct
+{
+	char	name[256];
+	int	age;
+	double	score;
+} student;
+/*
+int	main()
+{
+	//char 	src[] = "copy this";
+	//char 	dest[100];
+	student	student1;
+	//char	*test = "sai";
+	
+	strcpy(student1.name, "sai");
+	student1.age = 20;
+	student1.score = 99;
+
+	student	student2;
+	ft_memcpy(&student2, &student1, sizeof(student));
+
+	printf("name of student2 is %s\n", student2.name);
+}*/
