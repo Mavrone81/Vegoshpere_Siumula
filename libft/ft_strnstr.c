c0d73fc3a0a7ebfedc431381a548da600a415d0d
Samuel Fu <sfu@c2r4s14.42singapore.sg>
2023-09-23 20:51:42 +0800

    updated changes from up to strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: sfu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 14:19:58 by sfu               #+#    #+#             */
-/*   Updated: 2023/09/23 14:28:31 by sfu              ###   ########.fr       */
+/*   Updated: 2023/09/23 20:51:29 by sfu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,33 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
 {
-    size_t      needle_len;
-    const char  *haystack_ptr;
-    const char  *haystack_substr;
-    const char  *needle_substr;
+	size_t	len;
 
-    haystack_ptr = haystack;
-    needle_len = ft_strlen(needle);
-    if (needle_len == 0)
-        return (char *)haystack;
-    while (n > 0 && *haystack_ptr != '\0')
-    {
-        haystack_substr = haystack_ptr;
-        needle_substr = needle;
-        while (*haystack_substr == *needle_substr && *needle_substr != '\0' && n > 0)
-        {
-            haystack_substr++;
-            needle_substr++;
-            n--;
-        }
-        if (*needle_substr == '\0')
-            return((char *)haystack_ptr);
-    }
-    return(NULL);
-}
+	len = ft_strlen(needle);
+	if (!*needle)
+		return ((char *)haystack);
+	while (*haystack && n-- >= len)
+	{
+		if (*haystack == *needle)
+			return ((char *)haystack);
+		++haystack;
+	}
+	return(NULL);
+}/*
 int main() {
     const char *haystack = "Hello, World!";
-    const char *needle = "World";
+    const char *needle = "el";
     size_t len = 13; // Search within the first 13 characters
 
     char *result = ft_strnstr(haystack, needle, len);
 
     if (result != NULL) {
         write(1, "Substring found: ", 17);
-        write(1, result, 5); // Print the first 5 characters of the found substring
+        write(1, result, sizeof(result) +2); // Print the result
         write(1, "\n", 1);
     } else {
         write(1, "Substring not found\n", 21);
     }
 
     return 0;
-}
+}*/
