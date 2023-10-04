f384ef6fde5105d7c7a3690504e325dc4dd05634
Samuel Fu <sfu@c2r4s14.42singapore.sg>
2023-10-04 23:28:58 +0800

    updated changes from up to part2

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sfu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/01 15:08:40 by sfu               #+#    #+#             */
+/*   Updated: 2023/10/01 22:44:35 by sfu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
+{
+	size_t	len;
+	size_t	i;
+	char	*str;
+
+	i = 0;
+	if (!s || !f)
+		return (0);
+	len = ft_strlen(s);
+	if (!(str = (char *)malloc(len + 1)))
+		return(NULL);
+	while (i < len)
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
