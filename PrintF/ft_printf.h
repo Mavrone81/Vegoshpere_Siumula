88d1ee05288a8222b57eccf2f0302d84e3fbbed6
Samuel Fu <sfu@c1r4s4.42singapore.sg>
2024-01-08 03:44:53 +0800

    Submission V1.0.4 PrintF

--- PrintF/ft_printf.h
+++ PrintF/ft_printf.h
@@ -6,19 +6,19 @@
 /*   By: sfu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 02:53:32 by sfu               #+#    #+#             */
-/*   Updated: 2024/01/08 03:21:45 by sfu              ###   ########.fr       */
+/*   Updated: 2024/01/08 03:41:36 by sfu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdarg.h>
-#include <unistd.h>
-#include <limits.h>
-#include <string.h>
-#include <stdio.h>
+# include <stdarg.h>
+# include <unistd.h>
+# include <limits.h>
+# include <string.h>
+# include <stdio.h>
 
-int     ft_printf(const char *format, ...);
+int	ft_printf(const char *format, ...);
 
 #endif
